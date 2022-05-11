124063fb13a4ac2315a29676f3b8985e8e5caf55
Joshua Cheung <contact.cjoshua@gmail.com>
2022-05-11 15:48:29 -0700

    finished send string function

--- input.js
+++ input.js
@@ -29,6 +29,9 @@ const handleUserInput = function (key) {
   if (key === "d") {
     connection.write("Move: right");
   }
+  if (key === "h") {
+    connection.write("Say: Whaaaaaaaa");
+  }
 };
 
 module.exports = { setupInput };
