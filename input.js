7837aa7774e067230d2f1a0b4da1aa9706995fbf
Joshua Cheung <contact.cjoshua@gmail.com>
2022-05-11 15:45:47 -0700

    finished most of the function and able to move

---
+++ input.js
@@ -0,0 +1,34 @@
+let connection;
+
+const setupInput = function (conn) {
+  const stdin = process.stdin;
+  connection = conn;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
+    process.exit();
+    //conn.write("Move: up")
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+};
+
+module.exports = { setupInput };
---
+++ play.js
@@ -0,0 +1,8 @@
+const { connect } = require("./client.js");
+const { setupInput } = require("./input");
+// establishes a connection with the game server
+
+console.log("Connecting ...");
+let conn = connect();
+
+setupInput(conn);
